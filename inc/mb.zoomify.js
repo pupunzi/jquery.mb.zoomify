a19cdde8d278b22219abb7466c2d24525cb9dc6c
pupunzi <matteo@open-lab.com>
2013-01-04 17:35:15 +0100

    various

--- inc/mb.zoomify.js
+++ inc/mb.zoomify.js
@@ -1,23 +1,22 @@
 /*
  * ******************************************************************************
  *  jquery.mb.components
+ *  file: mb.zoomify.js
  *
- *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi); Open lab srl, Firenze - Italy
+ *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi);
+ *  Open lab srl, Firenze - Italy
  *  email: matteo@open-lab.com
- *  site: http://pupunzi.com
+ *  site: 	http://pupunzi.com
+ *  blog:	http://pupunzi.open-lab.com
+ * 	http://open-lab.com
  *
  *  Licences: MIT, GPL
  *  http://www.opensource.org/licenses/mit-license.php
  *  http://www.gnu.org/licenses/gpl.html
+ *
+ *  last modified: 04/01/13 17.13
  *  *****************************************************************************
  */
-
-/*
- * Name:jquery.mb.zoomify
- * Version: 1.3
- *
- */
-
 
 /*******************************************************************************
  * inclusion jquery.mousewheel.js
